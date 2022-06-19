1065d016ae1042b8a8e91890c55a35954ac2d904
Senopiece <shichiromail@gmail.com>
2022-06-19 12:53:53 +0300

    fork version

--- lib/scale_button.dart
+++ lib/scale_button.dart
@@ -1,43 +1,19 @@
 import 'package:flutter/material.dart';
 
 class ScaleButton extends StatefulWidget {
-  const ScaleButton({
-    Key? key,
-    Duration duration = const Duration(milliseconds: 300),
-    double bound = 0.2,
-    VoidCallback? onTap,
-    bool reverse = false,
-    required Widget child,
-  })  : this.duration = duration,
-        this.bound = bound,
-        this.onTap = onTap,
-        this.child = child,
-        this.reverse = reverse,
-        super(key: key);
-
-  /// Adjust the animation speed
-  ///
-  /// Defaults to [300]
   final Duration duration;
-
-  /// Adjust the shrink & zoom size.
-  ///
-  /// Default to [0.2]
-  final double bound;
-
-  /// Called when the user taps this part of the material.
   final VoidCallback? onTap;
-
-  /// The widget below this widget in the tree.
   final Widget child;
 
-  /// You can choose the type of animation [true : shrink, false: zoom]
-  ///
-  /// Default to [false]
-  final bool reverse;
+  const ScaleButton({
+    Key? key,
+    this.duration = const Duration(milliseconds: 40),
+    this.onTap,
+    required this.child,
+  }) : super(key: key);
 
   @override
-  _ScaleButtonState createState() => _ScaleButtonState();
+  State<ScaleButton> createState() => _ScaleButtonState();
 }
 
 class _ScaleButtonState extends State<ScaleButton>
@@ -46,18 +22,35 @@ class _ScaleButtonState extends State<ScaleButton>
   late AnimationController _controller;
 
   @override
-  void initState() {
-    super.initState();
-    _controller = AnimationController(
-      vsync: this,
-      duration: widget.duration,
-      upperBound: widget.bound,
-    )..addListener(() {
-        setState(() {
-          _scale =
-              widget.reverse ? 1 + _controller.value : 1 - _controller.value;
-        });
-      });
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTapDown: (_) {
+        _controller.animateTo(
+          0.1,
+          duration: widget.duration,
+          curve: Curves.easeInOutExpo,
+        );
+      },
+      onTapUp: (_) {
+        _controller.animateBack(
+          0,
+          duration: widget.duration,
+          curve: Curves.easeInOutExpo,
+        );
+      },
+      onTapCancel: () {
+        _controller.animateBack(
+          0,
+          duration: widget.duration,
+          curve: Curves.easeInOutExpo,
+        );
+      },
+      onTap: widget.onTap,
+      child: Transform.scale(
+        scale: _scale,
+        child: widget.child,
+      ),
+    );
   }
 
   @override
@@ -67,22 +60,16 @@ class _ScaleButtonState extends State<ScaleButton>
   }
 
   @override
-  Widget build(BuildContext context) {
-    return GestureDetector(
-      onTapDown: (_) {
-        _controller.forward();
-      },
-      onTapUp: (_) {
-        _controller.reverse();
-      },
-      onTapCancel: () {
-        _controller.reverse();
-      },
-      onTap: widget.onTap,
-      child: Transform.scale(
-        scale: _scale,
-        child: widget.child,
-      ),
-    );
+  void initState() {
+    super.initState();
+    _controller = AnimationController(
+      vsync: this,
+      duration: widget.duration,
+      upperBound: 0.1,
+    )..addListener(() {
+        setState(() {
+          _scale = 1 - _controller.value;
+        });
+      });
   }
 }
